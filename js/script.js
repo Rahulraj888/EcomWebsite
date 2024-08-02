3dfd36234cd2d0e4a33400d8d627c804072ae386
Rahulraj888 <reddavenirahul@gmail.com>
2024-08-02 12:44:03 -0400

    make header and footer dynamic

--- js/script.js
+++ js/script.js
@@ -1,32 +1,46 @@
 let cartItems = JSON.parse(localStorage.getItem('cartItems')) || [
     {
-        name: 'Laptop on Rent',
-        image: '/images/cart/c1.jpg',
-        quantity: 1,
-        price: 50
+      name: 'Laptop on Rent',
+      image: '/images/cart/c1.jpg',
+      quantity: 1,
+      price: 50
     }
-];
-
-
-function updateCartCount() {
+  ];
+  
+  function updateCartCount() {
     let cartCountElement = document.getElementById('cart-count');
-    cartCountElement.textContent = cartItems.length;
-}
-
-
-function addToCart(product) {
+    if (cartCountElement) {
+      cartCountElement.textContent = cartItems.length;
+    }
+  }
+  
+  function addToCart(product) {
     cartItems.push(product);
     localStorage.setItem('cartItems', JSON.stringify(cartItems));
     updateCartCount();
-}
-
-
-function removeFromCart(index) {
+  }
+  
+  function removeFromCart(index) {
     cartItems.splice(index, 1);
     localStorage.setItem('cartItems', JSON.stringify(cartItems));
     updateCartCount();
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-    updateCartCount(); 
-});
+  }
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    loadComponent('/html/header.html', 'header', updateCartCount);
+    loadComponent('/html/footer.html', 'footer');
+  });
+  
+//   call back mechanism to update cart
+  function loadComponent(url, elementId, callback = null) {
+    fetch(url)
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById(elementId).innerHTML = data;
+        if (callback) {
+          callback();
+        }
+      })
+      .catch(error => console.error('Error loading component:', error));
+  }
+  
